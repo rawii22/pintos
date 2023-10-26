29f75dcc95befa3b44f303fcf8cd84227fde53d0
Winter <77jamesthomas@gmail.com>
2023-10-25 21:42:13 -0700

    Ordered the semma waiting list

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,7 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered(&sema->waiters, &thread_current()->elem, thread_cmp_priority, NULL);
       thread_block ();
     }
   sema->value--;
@@ -113,11 +113,19 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+  if (!list_empty (&sema->waiters)){
+    /* Sorts the semaphore waiting list to account for updated priorities. */
+    list_sort(&sema->waiters, thread_cmp_priority, NULL);
+
+    thread_unblock (list_entry (list_pop_front (&sema->waiters), struct thread, elem));
+  }
   sema->value++;
   intr_set_level (old_level);
+
+  if (!intr_context())
+  {
+    thread_yield();
+  }
 }
 
 static void sema_test_helper (void *sema_);
